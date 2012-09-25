ccbacca6d4dbdde57e349275986b1441af3c83c7
Neal Lathia <neal.lathia@cl.cam.ac.uk>
2012-09-25 13:08:19 +0100

    Added buttons

---
+++ src/com/ubhave/sensormanager/tester/ApplicationContext.java
@@ -0,0 +1,22 @@
+package com.ubhave.sensormanager.tester;
+
+import android.app.Application;
+import android.content.Context;
+
+/**
+ * Application class to provide the global context.
+ */
+public class ApplicationContext extends Application
+{
+	private static ApplicationContext instance;
+
+	public ApplicationContext()
+	{
+		instance = this;
+	}
+
+	public static Context getContext()
+	{
+		return instance;
+	}
+}
--- src/com/ubhave/sensormanager/tester/MainActivity.java
+++ src/com/ubhave/sensormanager/tester/MainActivity.java
@@ -1,23 +1,68 @@
 package com.ubhave.sensormanager.tester;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.ubhave.sensormanager.ESException;
+import com.ubhave.sensormanager.ESSensorManager;
+import com.ubhave.sensormanager.ESSensorManagerInterface;
 
 public class MainActivity extends Activity
 {
-
+	
+	private final static String LOG_TAG = "MainActivity";
+	private ESSensorManagerInterface sensorManager;
+	
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		Log.d(LOG_TAG, "Getting Sensor Manager");
+		try {
+			sensorManager = ESSensorManager.getSensorManager(ApplicationContext.getContext());
+			enableStartSensingButton();
+			enableStopSensingButton();
+		}
+		catch(ESException e)
+		{
+			Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+		}
+		
+	}
+	
+	private void enableStartSensingButton()
+	{
+		Button button = (Button) findViewById(R.id.startSensing);
+		button.setOnClickListener(new OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				sensorManager.startAllSensors();
+			}
+		});
+	}
+	
+	private void enableStopSensingButton()
+	{
+		Button button = (Button) findViewById(R.id.startSensing);
+		button.setOnClickListener(new OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				sensorManager.stopAllSensors();
+			}
+		});
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu)
-	{
-		getMenuInflater().inflate(R.menu.activity_main, menu);
-		return true;
-	}
+	
 }
