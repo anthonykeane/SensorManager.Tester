26116a1743eaf313031221a905005619ffc83c12
Neal Lathia <neal.lathia@cl.cam.ac.uk>
2012-10-22 00:01:43 +0100

    Uncommitted changes

--- src/com/ubhave/sensormanager/tester/pullsensor/SensorDataUI.java
+++
@@ -1,8 +0,0 @@
-package com.ubhave.sensormanager.tester.pullsensor;
-
-import com.ubhave.sensormanager.data.SensorData;
-
-public interface SensorDataUI
-{
-	public void updateUI(SensorData data);
-}
