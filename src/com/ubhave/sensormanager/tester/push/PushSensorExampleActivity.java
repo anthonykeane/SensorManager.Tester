43c88940f5e2271d6b798b477d0f630b168a87d4
Neal Lathia <neal.lathia@cl.cam.ac.uk>
2012-11-06 14:35:20 +0000

    Minor updates

--- src/com/ubhave/sensormanager/tester/push/PushSensorExampleActivity.java
+++ src/com/ubhave/sensormanager/tester/push/PushSensorExampleActivity.java
@@ -1,13 +1,31 @@
 package com.ubhave.sensormanager.tester.push;
 
+import android.widget.EditText;
+
 import com.ubhave.sensormanager.tester.ExampleAbstractActivity;
 import com.ubhave.sensormanager.tester.R;
 
 public class PushSensorExampleActivity extends ExampleAbstractActivity
 {
-	
+
 	protected int getInterfaceLayout()
 	{
 		return R.layout.push_sensor_layout;
+	}
+
+	@Override
+	protected void setSensorDataField(String dataString)
+	{
+		EditText dataField = (EditText) findViewById(R.id.dataText);
+		String currentText = "";
+		try
+		{
+			currentText = dataField.getEditableText().toString();
+			dataField.setText(currentText + "\n" + dataString);
+		}
+		catch (NullPointerException e)
+		{
+			dataField.setText(dataString);
+		}
 	}
 }
